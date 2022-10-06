c1e41942788dc2f54ffad2b899c2928f21c6a3ef
psc-utb <psc.utb@gmail.com>
2022-10-06 17:06:13 +0200

    Area Admin a zobrazení CarouselItems pro správu

--- UTB.Eshop.Web/Startup.cs
+++ UTB.Eshop.Web/Startup.cs
@@ -48,6 +48,11 @@ namespace UTB.Eshop.Web
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
